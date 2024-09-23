8fb3b0750f155adb72b72c6c738e41c013e16b7d
FinlayDWebb <finlaywebb@rocketmail.com>
2024-09-23 16:55:45 +0100

    updated exercises

---
+++ ExercisesNotebook.hs
@@ -0,0 +1,41 @@
+-- We are just going to have rough equations in here, working through some questions. Primarily this GitHub will updated on Mondays, as this is the slot on my Google Calendar.
+-- Lets get started.
+
+-- Question 1, Chapter 5. Find a formula to find the point intersection of two lines.
+{-# LANGUAGE TemplateHaskell #-}
+import GHC.Base (divInt)
+import Data.Char (isDigit, digitToInt, toUpper)
+import GHC.Unicode (toLower)
+
+type Line = (Float, Float)
+
+intersect :: Line -> Line -> (Float, Float)
+intersect (a, b) (a', b') = (x, y)
+    where x = (b - b') / (a - a')
+          y = a * x + b
+
+-- Question 2, Chapter 5. Using list comprehension, write a function that returns half of each even number in a list.
+
+halveEvens :: [Int] -> [Int]
+halveEvens xs = [ divInt x 2 | x <- xs, even x]
+
+-- Question 4, Chapter 5. Using list comprehension, write a function that returns all numbers in the input list that fall within the range given by the first two parameters (inclusive).
+
+inRange :: Int -> Int -> [Int] -> [Int]
+inRange x y xs = [ a | a <- xs, a >= x, a <= y ] 
+
+-- Question 5, Chapter 5. Using list comprehension, write a function countPositive to count the number of positive numbers in a list (where 0 is positive).
+
+countPositive :: [Int] -> Int
+countPositive xs = sum [ 1 | x <- xs, x >= 0]
+
+-- Question 6, Chapter 5. Using list comprehension write a function multDigits that areutnrs the product of all the digits in the input. If no digits in the string, the function should return a 1.
+
+multDigits :: String -> Int
+multDigits str = product [ digitToInt x | x <- str, isDigit x]
+
+-- Question 7, Chapter 5. Using list comprehension, write an improved definition of the function capitalise. Note that it works with the 'map' addition, because this applies it to every element over and over in the section list.
+
+capitalise :: String -> String
+capitalise xs = toUpper (head xs) : map toLower (tail xs)
+
